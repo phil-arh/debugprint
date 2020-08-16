792876c0235f437ba99333d09f4d9582e8038654
Phil Howe <philhbusiness@gmail.com>
2020-08-16 17:21:07 +0100

    Add setup.py and setup.cfg

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="debugprint",
+    version="0.1.0",
+    license="Apache 2.0",
+    scripts=["src/debugprint.py"],
+    author="Phil Howe",
+    author_email="philhbusiness@gmail.com",
+    description="A clone of the npm debug module with ability to implement custom formatting",
+    keywords="debug print",
+    url="https://github.com/phil-arh/debugprint",
+)
